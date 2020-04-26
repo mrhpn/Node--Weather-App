f35b233982fe2595b4122c300e536d9c36494289
mrHtetPhyoNaing <mr.htetphyonaing@gmail.com>
2020-04-26 17:14:42 +0630

    -c styling

--- src/app.js
+++ src/app.js
@@ -1,34 +1,50 @@
 const path = require("path");
+const url = require("url");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
 // Paths to static assets and templates folder
 const publicDirectoryPath = `${__dirname}/../public`;
-const viewsPath = `${__dirname}/../templates`;
+const viewsPath = `${__dirname}/../templates/views`;
+const partialsPath = `${__dirname}/../templates/partials`;
 
 // Setting up handlebars template engine and views location
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Serving up static files
 app.use(express.static(publicDirectoryPath));
 
-app.get("", (req, res) => {
-  res.render("index", {
-    title: "Weather App",
-  });
-});
-
 app.get("/about", (req, res) => {
   res.render("about", {
-    title: "About Page",
+    title: "About",
+    about: url.parse(req.url, true).path.includes("about") ? "active" : "",
+    footerTitle: "Developed by HPN",
   });
 });
 
 app.get("/help", (req, res) => {
   res.render("help", {
-    title: "Help Page",
+    title: "Help",
+    help: url.parse(req.url, true).path.includes("help") ? "active" : "",
+    footerTitle: "Developed by HPN",
+  });
+});
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    home: "active",
+    footerTitle: "Developed by HPN",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404 Page Not Found",
   });
 });
 
