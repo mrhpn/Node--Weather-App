06de765d2581d394f3a2182f5fc6e734e046b9ba
mrHtetPhyoNaing <mr.htetphyonaing@gmail.com>
2020-04-26 13:59:53 +0630

    changed views route

--- src/app.js
+++ src/app.js
@@ -3,11 +3,16 @@ const express = require("express");
 
 const app = express();
 
-// Setting up handlebars template engine
+// Paths to static assets and templates folder
+const publicDirectoryPath = `${__dirname}/../public`;
+const viewsPath = `${__dirname}/../templates`;
+
+// Setting up handlebars template engine and views location
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
 
 // Serving up static files
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
   res.render("index", {
