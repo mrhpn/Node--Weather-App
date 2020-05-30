17db8eb3d48c92b0efd7ca73369825f0f1b12f6c
Htet Phyo Naing <mailtohtetphyonaing@gmail.com>
2020-05-30 15:54:07 +0630

    add readme.md

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-readme
+# Weather App
+
+There are a lots of apps and widgets out there associated with weather forecasting app and here is the next one.
+
+My purpose on this app is simply just to learn nodejs and express.
+
+See [live](https://phyo-weather.herokuapp.com).
+
+## Used Technologies
+
+`hbs` for template engine, `express`, `mapbox` for getting geolocations and `darksky` for weather forecasting task.
+
+## How It Looks
+
+![Home](https://i.ibb.co/wh4bNzf/look.png)
--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,21 @@
+/***************************************
+ * Skycons Configurations
+ */
+var icons = new Skycons({ color: "orange" });
+
+icons.set("clear-day", Skycons.CLEAR_DAY);
+icons.set("clear-night", Skycons.CLEAR_NIGHT);
+icons.set("partly-cloudy-day", Skycons.PARTLY_CLOUDY_DAY);
+icons.set("partly-cloudy-night", Skycons.PARTLY_CLOUDY_NIGHT);
+icons.set("cloudy", Skycons.CLOUDY);
+icons.set("rain", Skycons.RAIN);
+icons.set("sleet", Skycons.SLEET);
+icons.set("snow", Skycons.SNOW);
+icons.set("wind", Skycons.WIND);
+icons.set("fog", Skycons.FOG);
+
+icons.play();
+
 const domString = {
   searched: ".searched",
   result: ".result",
@@ -71,21 +89,3 @@ locationForm.addEventListener("submit", (e) => {
     });
   });
 });
-
-/***************************************
- * Skycons Configurations
- */
-var icons = new Skycons({ color: "orange" });
-
-icons.set("clear-day", Skycons.CLEAR_DAY);
-icons.set("clear-night", Skycons.CLEAR_NIGHT);
-icons.set("partly-cloudy-day", Skycons.PARTLY_CLOUDY_DAY);
-icons.set("partly-cloudy-night", Skycons.PARTLY_CLOUDY_NIGHT);
-icons.set("cloudy", Skycons.CLOUDY);
-icons.set("rain", Skycons.RAIN);
-icons.set("sleet", Skycons.SLEET);
-icons.set("snow", Skycons.SNOW);
-icons.set("wind", Skycons.WIND);
-icons.set("fog", Skycons.FOG);
-
-icons.play();
